c4916b75c0742345029ed9d59323943731d8ab8d
wangny <angelbubbleonly@gmail.com>
2018-06-05 17:04:25 +0800

    tunnig for better result

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ import util
 
 
 x, sr = util.load_audio('data/Pirates_of_the_Caribbean.wav')
-chromagram = librosa.feature.chroma_stft(y=x, sr=sr, norm=None, base_c = False )
+chromagram = librosa.feature.chroma_stft(y=x, sr=sr, norm=2, hop_length=128, base_c = False )
 
 chroma = np.argmax( chromagram, axis=0)
 maxch = np.max(chromagram, axis=0)
 for i in range(len(chroma)):
-    if maxch[i] < 0.5:
+    if maxch[i] <= 0.8:
         print(util.note[-1], end=' ')
     else:
         print(util.note[chroma[i]], end=' ')
