3512802c813f2b615b4ee28678ca072e27341dfa
wangny <angelbubbleonly@gmail.com>
2018-06-05 17:19:27 +0800

    sort note and length to an array

--- main.py
+++ main.py
@@ -15,8 +15,26 @@ chromagram = librosa.feature.chroma_stft(y=x, sr=sr, norm=2, hop_length=128, bas
 
 chroma = np.argmax( chromagram, axis=0)
 maxch = np.max(chromagram, axis=0)
+
+record = []
+current = None
+count = 0
 for i in range(len(chroma)):
+    next_note = None
     if maxch[i] <= 0.8:
-        print(util.note[-1], end=' ')
+#        print(util.note[-1], end=' ')
+        next_note = util.note[-1]
     else:
-        print(util.note[chroma[i]], end=' ')
+#        print(util.note[chroma[i]], end=' ')
+        next_note = util.note[chroma[i]]
+    if current == next_note or current is None:
+        current = next_note
+        count += 1
+    else:
+        record.append((current, count))
+        current = next_note
+        count = 1
+record.append((current, count))
+
+for a in record:
+    print(a, end=' ')
